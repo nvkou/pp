25389ec23c06694a6265711003af2249ebb5af27
gordon <gang@o2oworldwide.com>
2018-03-30 14:51:06 -0600

    添加项目文件。

---
+++ pp/IChecksum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pp
+{
+    interface IChecksum
+    {
+        string Generate_checksum(Dictionary<string, string> parameter);
+        bool Vaileade_checksum(Dictionary<string, string> parameter, string hmac);
+    }
+}
---
+++ pp/rClient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace pp
+{
+    class rClient
+    {
+        private RestClient Rclient;
+        public string url;
+        public string header;
+        public string result;
+        public Dictionary<string, string> pararmeter;
+        public string error;
+        public Method method;
+        public Exception exception;
+ 
+
+        public rClient(string url,Method method,Dictionary<string,string> parameter=null) {
+            this.Rclient = new RestClient(url);
+        }
+
+
+
+        
+        
+    }
+}
